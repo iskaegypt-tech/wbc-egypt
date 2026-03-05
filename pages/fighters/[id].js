0a706300f2d05a20571d7347ae36da2d0680c978
iskaegypt-tech <iska.egypt@gmail.com>
2026-03-05 19:30:48 +1000

    Update [id].js

--- pages/fighters/[id].js
+++ pages/fighters/[id].js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import supabase from "../../lib/supabaseClient";
 
-export default function FighterProfile() {
+export default function FighterPage() {
 
   const router = useRouter();
   const { id } = router.query;
@@ -21,9 +21,7 @@ export default function FighterProfile() {
         .eq("id", id)
         .single();
 
-      if (!error) {
-        setFighter(data);
-      }
+      if (!error) setFighter(data);
 
     }
 
@@ -31,45 +29,28 @@ export default function FighterProfile() {
 
   }, [id]);
 
-  if (!fighter) {
-    return <p>Loading...</p>;
-  }
+  if (!fighter) return <p>Loading...</p>;
 
   return (
 
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+    <div style={{padding:"40px"}}>
+
+      <h1>{fighter.full_name}</h1>
 
       {fighter.photo_url && (
         <img
           src={fighter.photo_url}
-          style={{
-            width: "200px",
-            borderRadius: "10px",
-            marginBottom: "20px"
-          }}
+          width="250"
+          style={{marginBottom:"20px"}}
         />
       )}
 
-      <h1>{fighter.full_name}</h1>
-
-      <p>
-        <b>Nickname:</b> {fighter.nickname}
-      </p>
-
-      <p>
-        <b>Nationality:</b> {fighter.nationality}
-      </p>
-
-      <p>
-        <b>Weight Class:</b> {fighter.weight_class}
-      </p>
-
-      <p>
-        <b>Record:</b> {fighter.wins}-{fighter.losses}-{fighter.draws}
-      </p>
+      <p><b>Nickname:</b> {fighter.nickname}</p>
+      <p><b>Nationality:</b> {fighter.nationality}</p>
+      <p><b>Weight Class:</b> {fighter.weight_class}</p>
+      <p><b>Record:</b> {fighter.wins}-{fighter.losses}-{fighter.draws}</p>
 
     </div>
 
   );
-
 }
