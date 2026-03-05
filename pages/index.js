059263da2887c730013d694053491df097eb930a
iskaegypt-tech <iska.egypt@gmail.com>
2026-03-05 12:18:15 +1000

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily:'Arial', textAlign:'center', marginTop:'100px'}}>
+      <h1>WBC Muay Thai Egypt</h1>
+      <p>Official Fighters & Events Management Platform</p>
+    </div>
+  );
+}
