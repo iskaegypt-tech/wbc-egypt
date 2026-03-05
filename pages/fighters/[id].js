236b60d6a945e11aac9023663493e208b6d73b8b
iskaegypt-tech <iska.egypt@gmail.com>
2026-03-05 18:24:50 +1000

    Update [id].js

--- pages/fighters/[id].js
+++ pages/fighters/[id].js
@@ -15,34 +15,43 @@ export default function FighterPage() {
 
   async function fetchFighter() {
 
-    const { data, error } = await supabase
+    const { data } = await supabase
       .from("fighters")
       .select("*")
       .eq("id", id)
       .single();
 
-    if (!error) {
-      setFighter(data);
-    }
+    setFighter(data);
   }
 
   if (!fighter) return <p>Loading fighter...</p>;
 
   return (
-    <div style={{ padding: "40px" }}>
+    <div style={{padding:"40px", fontFamily:"Arial"}}>
 
       <h1>{fighter.full_name}</h1>
 
-      <p><b>Nickname:</b> {fighter.nickname}</p>
+      <div style={{
+        border:"1px solid #ccc",
+        padding:"20px",
+        width:"400px"
+      }}>
 
-      <p><b>Nationality:</b> {fighter.nationality}</p>
+        <p><b>Nickname:</b> {fighter.nickname}</p>
 
-      <p><b>Weight Class:</b> {fighter.weight_class}</p>
+        <p><b>Nationality:</b> {fighter.nationality}</p>
 
-      <p>
-        <b>Record:</b> {fighter.wins}-{fighter.losses}-{fighter.draws}
-      </p>
+        <p><b>Weight Class:</b> {fighter.weight_class}</p>
+
+        <p>
+          <b>Record:</b> {fighter.wins}-{fighter.losses}-{fighter.draws}
+        </p>
+
+      </div>
+
+      <br/>
+
+      <a href="/fighters">← Back to Fighters</a>
 
     </div>
   );
-}
