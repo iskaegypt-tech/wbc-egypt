cd2c5450c646283dbbcd8692b37e966319e0570e
iskaegypt-tech <iska.egypt@gmail.com>
2026-03-05 20:47:10 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,71 +2,70 @@ import Link from "next/link";
 
 export default function Home() {
 return (
+<div style={{ fontFamily: "Arial", textAlign: "center" }}>
 
-<div style={{fontFamily:"Arial", textAlign:"center"}}>
-
+```
   {/* Header */}
-  <div style={{
-    background:"#000",
-    color:"#fff",
-    padding:"40px"
-  }}>
+  <div
+    style={{
+      background: "#000",
+      color: "#fff",
+      padding: "40px"
+    }}
+  >
 
-    <div style={{
-      display:"flex",
-      alignItems:"center",
-      justifyContent:"center",
-      gap:"20px"
-    }}>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "20px"
+      }}
+    >
 
       <img
         src="https://vnagfyicoofgqwktrsvu.supabase.co/storage/v1/object/public/Public/WBC%20Muay%20Thai%20Egypt.jpg"
-        style={{width:"160px"}}
+        alt="WBC Belt"
+        style={{ width: "160px" }}
       />
 
-      <h1 style={{margin:0}}>
+      <h1 style={{ margin: 0 }}>
         WBC Muay Thai Egypt
       </h1>
 
     </div>
 
-    <p style={{marginTop:"10px"}}>
+    <p style={{ marginTop: "10px" }}>
       Official Fighters & Events Management Platform
     </p>
 
   </div>
 
-
   {/* Navigation */}
-  <div style={{marginTop:"60px"}}>
+  <div style={{ marginTop: "60px" }}>
 
     <h2>Explore the Platform</h2>
 
-    <div style={{
-      display:"flex",
-      justifyContent:"center",
-      gap:"30px",
-      marginTop:"30px"
-    }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        gap: "30px",
+        marginTop: "30px"
+      }}
+    >
 
-      <Link href="/fighters">
-        Fighters
-      </Link>
-
-      <Link href="/events">
-        Events
-      </Link>
-
-      <Link href="/titles">
-        Titles
-      </Link>
-
-      <Link href="/rankings">
-        Rankings
-      </Link>
+      <Link href="/fighters">Fighters</Link>
+      <Link href="/events">Events</Link>
+      <Link href="/titles">Titles</Link>
+      <Link href="/rankings">Rankings</Link>
 
     </div>
 
   </div>
 
 </div>
+```
+
+);
+}
