f9c9f09d1dd5cda31e303f66813c58226eb88346
iskaegypt-tech <iska.egypt@gmail.com>
2026-03-05 12:29:09 +1000

    Create fighter-register.js

---
+++ pages/fighter-register.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function FighterRegister() {
+  const [name, setName] = useState("");
+  const [weight, setWeight] = useState("");
+  const [gym, setGym] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    alert("Fighter submitted (database connection will be next step)");
+  };
+
+  return (
+    <div style={{textAlign:"center",marginTop:"50px"}}>
+      <h1>Fighter Registration</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Fighter Name"
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+        />
+        <br/><br/>
+
+        <input
+          placeholder="Weight Class"
+          value={weight}
+          onChange={(e)=>setWeight(e.target.value)}
+        />
+        <br/><br/>
+
+        <input
+          placeholder="Gym Name"
+          value={gym}
+          onChange={(e)=>setGym(e.target.value)}
+        />
+        <br/><br/>
+
+        <button type="submit">Register Fighter</button>
+      </form>
+    </div>
+  );
+}
