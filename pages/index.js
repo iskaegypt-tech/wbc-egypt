aa209c6e4bc6dd0ff9a94466f30029510183433a
iskaegypt-tech <iska.egypt@gmail.com>
2026-03-05 20:20:25 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div style={{fontFamily:'Arial', textAlign:'center', marginTop:'100px'}}>
+    <div style={{textAlign:"center", marginTop:"80px"}}>
+
       <h1>WBC Muay Thai Egypt</h1>
       <p>Official Fighters & Events Management Platform</p>
+
+      <div style={{marginTop:"40px", fontSize:"20px"}}>
+
+        <p>
+          <Link href="/fighters">Fighters</Link>
+        </p>
+
+        <p>
+          <Link href="/events">Events</Link>
+        </p>
+
+        <p>
+          <Link href="/titles">Titles</Link>
+        </p>
+
+        <p>
+          <Link href="/rankings">Rankings</Link>
+        </p>
+
+      </div>
+
     </div>
   );
 }
